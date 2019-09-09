e7120c4ea009044fc379ad4e8e1cfb141ada1edb
dom061077 <dom061077@gmail.com>
2019-09-09 14:08:26 -0300

    security

--- grails-app/init/schoolmanagment/BootStrap.groovy
+++ grails-app/init/schoolmanagment/BootStrap.groovy
@@ -9,8 +9,11 @@ class BootStrap {
 
     def init = { servletContext ->
         def role1 = new Authority(authority:"ROLE_USER").save flush:true,failOnError:true
-        def user1 = new User(username:"user1",password:"user1").save flush:true,failOnError:true
+        def user1 = new User(username:"user1",password:"user1",apellido:"Apellido 1",nombre:"Nombre 1").save flush:true,failOnError:true
         UserAuthority.create(user1,role1)       
+        
+        user1 = new User(username:"user2",password:"user2",apellido:"Apellido 2",nombre:"Nombre 2").save flush:true, failOnError:true
+        UserAuthority.create(user1,role1)
         
         new Alumno("apellido": "Gallardo","nombre":"Marcelo").save()
         new Alumno("apellido": "Francescoli","nombre":"Enzo").save()
