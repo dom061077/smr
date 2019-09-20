35cf3299b753d1da8d7e0dee8beeff14c292f154
dom061077 <dom061077@gmail.com>
2019-09-20 20:17:50 -0300

    alumno

--- grails-app/domain/com/smr/alumno/Alumno.groovy
+++ grails-app/domain/com/smr/alumno/Alumno.groovy
@@ -10,6 +10,37 @@ class Alumno {
     String apellido
     String nombre
     java.sql.Date fechaNacimiento
+    int dni
+    String cuil
+    String direccion
+    boolean planSocial
+    boolean trabaja
+    
+    String apellidoTutor
+    String nombreTutor
+    String parentescoTutor
+    int dniTutor
+    String cuilTutor
+    String telefono1
+    String telefono2
+    boolean fotoDni
+    boolean constanciaCuil
+    boolean constancia6grado
+    boolean actaNacimiento
+    boolean constaciaRegular
+    boolean foto4x4
+    boolean fotoCarnetVac
+    boolean fichaMedica
+    boolean aptitudFisica
+    boolean grupoSanguineo
+    boolean fichaInscripcion
+    boolean libreta6grado
+    boolean fotocopiaLibroMatriz
+    boolean fotocopiaDniTutor
+    boolean constanciaCuilTutor
+    
+    
+    
     
     static constraints = {
         fechaNacimiento(nullable:true, blank:true)
