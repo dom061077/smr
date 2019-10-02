7c232a15e5c14cd16d0839a56fc637ae4ad4aaa3
dom061077 <dom061077@gmail.com>
2019-10-02 19:08:54 -0300

    alumno

--- grails-app/controllers/com/smr/alumno/AlumnoController.groovy
+++ grails-app/controllers/com/smr/alumno/AlumnoController.groovy
@@ -4,6 +4,7 @@ package com.smr.alumno
 import grails.rest.*
 import grails.converters.*
 import grails.web.JSONBuilder
+import java.text.SimpleDateFormat
 
 class AlumnoController {
 	static responseFormats = ['json', 'xml']
@@ -18,7 +19,24 @@ class AlumnoController {
     def save(){
         log.info("Parametros de save alumno: "+request.JSON)
         def alumnoInstance = new Alumno(request.JSON)
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        Date date = sdf.parse(request.JSON.fechaNacimientoUnbinding)
+        log.info("Date java.util: "+date)
+        alumnoInstance.fechaNacimiento = new java.sql.Date(date.getTime())
+        
         log.info("Alumno Instance: "+alumnoInstance.properties)
+        
+        if(alumnoInstance.hasErrors()){
+            alumnoInstance.errors.each{
+                log.debug('ERROR!!!: '+it)
+            }
+            render (view:"/errors/_errors",model:[errors:alumnoInstance.errors])
+            return            
+        }
+        
+        alumnoInstance.save(flush:true)
+        
         JSONBuilder jsonBuilder = new JSONBuilder()
         def json = jsonBuilder.build{
             success = true
